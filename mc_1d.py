94527f706372114e3e1639f28a98a1930750fc8a
jakegrigsby <jcg6dn@virginia.edu>
2021-04-12 22:43:26 -0400

    remove extra mc_1d code

--- mc_1d.py
+++ mc_1d.py
@@ -22,32 +22,3 @@ def load_mc1d():
 
     env = dc.envs.load_gym("MountainCarContinuous-v0")
     return MC1DWrapper(env)
-
-
-if __name__ == "__main__":
-    import argparse
-    import deep_control as dc
-    import efr, efr2
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--agent", type=str, required=True)
-    args = parser.parse_args()
-
-    test_env = load_mc1d()
-    state_space = test_env.observation_space
-    action_space = test_env.action_space
-
-    # create agent
-    agent = efr2.EFRAgent(
-        state_space.shape[0],
-        action_space.shape[0],
-        -10.0,
-        2.0,
-        2,
-        advantage_method="mean",
-        use_popart=True,
-    )
-
-    agent.load(args.agent)
-
-    dc.run.evaluate_agent(agent, test_env, 20, 1000, True)
